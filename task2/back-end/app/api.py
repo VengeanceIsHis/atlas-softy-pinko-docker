c387ad49352d40ddbfc97e9a0e3df6a8d0261340
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-06-24 13:34:03 -0500

    fixing api to display on home screen

--- task2/back-end/app/api.py
+++ task2/back-end/app/api.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Welcome to my Flask application!'
+
 @app.route('/api/hello')
 def hello_world():
     return 'Hello, World!'
--- task3/back-end/api.py
+++ task3/back-end/api.py
@@ -4,6 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def index():
+    return 'Welcome to my Flask application!'
+
 @app.route('/api/hello')
 def hello_world():
     return 'Hello, World!'
